205b429006f17d2ccdcbb1025a94e29d5020f916
William <william.lnx@gmail.com>
2020-05-08 17:12:14 -0500

    Add content to constructor in PDEsolver

--- PDEsolver.py
+++ PDEsolver.py
@@ -8,7 +8,33 @@ from dolfin import *
 
 class PDEsolver():
 
-	def __init__(self,domain, time, Mi, v_=None):
+	def __init__(self,domain, time, Mi, v_=None,params=None):
+		# inptu store
+		self._domain = domain 
+		self._Mi = Mi 
+		self._time = time 
+
+		if params != None
+			self.params = params 
+		else:
+			self.params = self.default_parameters
+
+		# set up function space
+		# need to implement change order (k)
+		k = 1
+		V = FunctionSpace(self._domain,"CG",k) 
+		self.V = V
+
+		if v_ != None:
+			self.v_ = Function(self.V,name='v_')
+		else
+			self.v_ = v_
+		self.v = Function(V,name="v")
+
 	def step(self,interval):
+		"""
+		solution at one step
+		
+		"""
 	def default_parameters():
 	def solve():
