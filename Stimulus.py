d57b67b5d94595421c878a49038e89e53ae6cd72
William <william.lnx@gmail.com>
2020-05-12 16:50:34 -0500

    Added content rhs(). Define get values and keys of objects

--- Stimulus.py
+++ Stimulus.py
@@ -2,8 +2,23 @@
 Class to handle stimulus
 
 """
+from dolfin import *
 
 class Stimulus():
 	def __init__(self,objects,keys,markers):
 		self._objects = dict(zip(keys,objects))
-	def rhs()
+		self._markers = markers
+
+	def keys(self):
+		return self._objects.keys()
+
+	def values():
+		return self._objects.values()
+
+	def rhs(self,mesh,v):
+		markers = self._markers()
+		dz = Measure("dx", domain=mesh, subdomain_data=markers)
+		rhs = sum([g*v*dz(i) for (i, g) in zip(self.keys(), self.values())])
+				
+		return (dz, rhs)	
+
