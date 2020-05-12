70a4028c99e55538afe42eeddfe69f01cf59f0f7
William <william.lnx@gmail.com>
2020-05-12 14:24:55 -0500

    Add ODESolver.py with defined members

---
+++ ODESolver.py
@@ -0,0 +1,19 @@
+
+"""
+ODE solver 
+
+"""
+from dolfin import *
+
+class ODESolver():
+	def __init__(self,mesh,time,model,I_s=None,params=None):
+	def time(self):
+		pass
+	def step(self,interval):
+		pass
+	def solution_fields(self):
+		pass
+	def default_parameters(self):
+		pass
+	def solve():
+		pass
