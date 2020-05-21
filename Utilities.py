647667a8a3d5adcb2f6adec8e52f0ba170e67a99
William <william.lnx@gmail.com>
2020-05-21 17:38:00 -0500

    test_single_cell.py and Utilities.py added

---
+++ Utilities.py
@@ -0,0 +1,124 @@
+"""
+Utilities
+
+"""
+import dolfin 
+
+class TimeStepper:
+    """
+    A helper object that keep track of simulated time
+    """
+    def __init__(self, interval, dt):
+        """
+        
+        """
+
+        
+        if not isinstance(interval, (tuple, list)) or len(interval) != 2 or \
+               not all(isinstance(value, (float, int)) for value in interval):
+            raise TypeError("expected tuple or list of size 2 with scalars for "\
+                            "the interval argument")
+
+        if interval[0] >= interval[1]:
+            raise ValueError("Start time need to be larger than stop time: "\
+                             "interval[0] < interval[1]")
+
+        # Store time interval
+        (self.T0, self.T1) = interval
+
+        if not isinstance(dt, (float, int, list)):
+            raise TypeError("expected float or list of tuples for dt argument")
+
+        if isinstance(dt, (float, int)):
+            dt = [(self.T0, dt)]
+
+        # Check that all dt are tuples of size 2 with either floats or ints.
+        if any((not isinstance(item, tuple) or \
+                len(item)!=2 or not all(isinstance(value, (float, int)) \
+                                        for value in item)) for item in dt):
+            raise TypeError("expected list of tuples of size 2 with scalars for "\
+                            "the dt argument")
+
+        # Check that first time value of dt is the same as the first given in interval
+        if dt[0][0] != self.T0:
+            raise ValueError("expected first time value of dt to be the same as "\
+                             "the first value of time interval.")
+
+        # Check that all time values given in dt are increasing
+        if not all(dt[i][0] < dt[i+1][0] for i in range(len(dt)-1)):
+            raise ValueError("expected all time values in dt to be increasing")
+
+        # Check that all time step values given in dt are positive
+        if not all(dt[i][1] > 0 for i in range(len(dt))):
+            raise ValueError("expected all time step values in dt to be positive")
+
+        # Store dt
+        self._dt = dt
+
+        # Add a dummy dt including stop interval time
+        if self._dt[-1][0] < self.T1:
+            self._dt.append((self.T1, self._dt[-1][1]))
+
+        # Keep track of dt index
+        self._dt_ind = 0
+        self.t0 = self.T0
+
+    def __iter__(self):
+        """
+        Return an iterator over time intervals
+        """
+        eps = 1e-10
+
+        while True:
+
+            # Get next t1
+            t1 = self.next_t1()
+
+            # Yield time interval
+            yield self.t0, t1
+
+            # Break if this is the last step
+            if abs(t1-self.T1) < eps:
+                break
+
+            # Move to next time
+            self.t0 = t1
+
+    def next_t1(self):
+        """
+        Return the time of next end interval
+        """
+        assert self._dt_ind < len(self._dt)+1
+        dt = self._dt[self._dt_ind][1]
+        time_to_switch_dt = self._dt[self._dt_ind+1][0]
+        if time_to_switch_dt - dolfin.DOLFIN_EPS > self.t0 + dt :
+            return self.t0 + dt
+
+        # Update dt index
+        self._dt_ind += 1
+        return time_to_switch_dt
+
+
+def state_space(domain, d, family=None, k=1):
+    """Return function space for the state variables.
+
+    *Arguments*
+      domain (:py:class:`dolfin.Mesh`)
+        The computational domain
+      d (int)
+        The number of states
+      family (string, optional)
+        The finite element family, defaults to "CG" if None is given.
+      k (int, optional)
+        The finite element degree, defaults to 1
+
+    *Returns*
+      a function space (:py:class:`dolfin.FunctionSpace`)
+    """
+    if family is None:
+        family = "CG"
+    if d > 1:
+        S = dolfin.VectorFunctionSpace(domain, family, k, d)
+    else:
+        S = dolfin.FunctionSpace(domain, family, k)
+    return S
---
+++ test_single_cell.py
@@ -0,0 +1,73 @@
+"""
+test Single Cell
+
+"""
+
+import matplotlib.pyplot as plt
+from dolfin import *
+from ODESolver import SingleCellSolver
+from cell_models.Fenton_Karma_BR_altered import Fenton_Karma_1998_BR_altered
+
+# Turn on FFC/FEniCS optimizations
+parameters["form_compiler"]["representation"] = "uflacs"
+parameters["form_compiler"]["cpp_optimize"] = True
+flags = ["-O3", "-ffast-math", "-march=native"]
+parameters["form_compiler"]["cpp_optimize_flags"] = " ".join(flags)
+parameters["form_compiler"]["quadrature_degree"] = 3
+
+class MyExpression(UserExpression):
+	def __init__(self,period,duration,start,Iamp,t,**kwargs):
+		self.period = period
+		self.duration = duration
+		self.start = start
+		self.Iamp = Iamp
+		self.t = t
+		super().__init__(self,**kwargs)
+	def eval(self,value,x):
+		t = float(self.t)
+		I =(self.Iamp if t-int(t/self.period)*self.period  <= self.duration+self.start else 0.0) \
+			if (t-int(t/self.period)*self.period >= self.start) else 0.0*self.Iamp
+		value[0] = I
+
+
+time = Constant(0.0)
+
+# Surface to volume ratio
+chi = 140.0     # mm^{-1}
+# Membrane capacitance
+C_m = 0.01 # mu F / mm^2
+duration = 5. # ms
+A = 25000. # mu A/cm^3
+cm2mm = 10.
+factor = 1.0/(chi*C_m) # NB: cbcbeat convention
+amplitude = factor*A*(1./cm2mm)**3 # mV/ms
+start = 2.0
+period = 200.0
+
+I_s = MyExpression(period,duration,start,amplitude,time)
+
+
+cell_model = Fenton_Karma_1998_BR_altered()
+cell_solver = SingleCellSolver(cell_model,time,I_s)
+
+dt = 0.1
+interval = (0.0,400.0)
+voltage = []
+time = []
+printstep = 10.0
+count = 0
+
+for (timestep,fields) in cell_solver.solve(interval,dt):
+
+		(vs_,vs) =fields
+		(t0,t) = timestep
+		if float(count)%printstep == 0:
+			voltage.append(vs.vector().get_local()[0])
+			time.append(t)
+		count += 1
+		
+		print("(t_0, t_1) = (%g, %g)"%timestep)
+
+plt.figure()
+plt.plot(time,voltage,lw=2)
+plt.show()
